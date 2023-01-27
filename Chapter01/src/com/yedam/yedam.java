898f546e603993081986747448580708046b6a6c
admin <admin@YD05-12>
2023-01-27 16:17:36 +0900

    java 공부

---
+++ Chapter01/src/com/yedam/yedam.java
@@ -0,0 +1,13 @@
+package com.yedam;
+
+public class yedam {
+	
+	// 기본 자바 실행할때 쓰는 함수(메소드)
+	//public static void main(String[] args) {}
+	
+	
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
+
+}
